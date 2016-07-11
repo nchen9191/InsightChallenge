0a0c8e1a4fd39fc05fb19b2eb96f4c17444eb604
nchen9191 <nchen9191@gmail.com>
2016-07-11 16:19:41 -0700

    Add files via upload

--- InsightChallenge/run.sh
+++ InsightChallenge/run.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-# example of the run script for running the rolling_median calculation with a python file, 
-# but could be replaced with similar files from any major language
-
-# I'll execute my programs, with the input directory venmo_input and output the files in the directory venmo_output
+# Execute rolling median.py with input venmo-trans.txt and output to /venmo_output/output.txt
 python ./src/rolling_median.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
 
 
